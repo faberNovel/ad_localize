76247fd86cec754d5617ffec65479a10ba651433
Joanna Vigne <joanna.vigne@applidium.com>
2017-06-09 12:30:23 +0200

    [Android fix] add support for formats with numbers between $ and i (ex: %01i and %1$02i)
    Change-Id: I9fc9a4584d999c6bed652d32b67cdaf0bb2246d8

--- lib/internationalize/platform_formatters/android_formatter.rb
+++ lib/internationalize/platform_formatters/android_formatter.rb
@@ -45,9 +45,9 @@ module Internationalize::Platform
       processedValue = processedValue.gsub(/(?<!\\)'/, "\\\\'")
       processedValue = processedValue.gsub(/(?<!\\)\"/, "\\\"")
       processedValue = processedValue.gsub(/&(?!(?:amp|lt|gt|quot|apos);)/, '&amp;')
-      processedValue = processedValue.gsub(/(%(\d+\$)?@)/, '%\2s')
-      processedValue = processedValue.gsub(/(%(\d+\$)?i)/, '%\2d')
-      processedValue = processedValue.gsub(/%(?!(\d+\$)?[sd])/, '%%')
+      processedValue = processedValue.gsub(/(%(\d+\$)?@)/, '%\2s') # should match values like %1$s and %s
+      processedValue = processedValue.gsub(/(%((\d+\$)?(\d+)?)i)/, '%\2d') # should match values like %i, %3$i, %01i, %1$02i
+      processedValue = processedValue.gsub(/%(?!((\d+\$)?(s|(\d+)?d)))/, '%%') # negative lookahead: identifies when user really wants to display a %
       processedValue = processedValue.gsub("\\U", "\\u")
       value = "\"#{processedValue}\""
     end
