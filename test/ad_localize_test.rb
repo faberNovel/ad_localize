6557bbff3dd26268d0020063e5279d524d8ef9f2
s.jcqs <satyan.jacquens@fabernovel.com>
2020-05-19 19:46:52 +0200

    Add multi export test

--- test/ad_localize_test.rb
+++ test/ad_localize_test.rb
@@ -11,7 +11,7 @@ class AdLocalizeTest < TestCase
 
   def teardown
     # Clean up
-    FileUtils.rm_rf('exports')
+    #FileUtils.rm_rf('exports')
   end
 
   test 'it has a version number' do
@@ -37,6 +37,7 @@ class AdLocalizeTest < TestCase
       diff = Diffy::Diff.new(reference_file, generated_file, :source => 'files')
       assert_empty(diff.to_s, "File #{generated_file} do not match reference. Diff: \n\n#{diff}\n")
     end
+    FileUtils.rm_rf('exports')
   end
 
   test 'it should not create intermediate platform directory' do
@@ -55,15 +56,50 @@ class AdLocalizeTest < TestCase
       generated_file = "exports/#{file}"
       assert(File.exist?(generated_file), "File does not exists #{generated_file}")
     end
+    FileUtils.rm_rf('exports')
+  end
+
+  test 'it should export multiple input csv files' do
+      # Given
+      csv_files = ['test/reference.csv', 'test/reference1.csv', 'test/reference2.csv']
+      csv_files.each do |file|
+        assert(File.exist?(file), "File does not exists #{file}")
+      end
+      reference_dir = "test/exports_reference_multi"
+      assert(File.exist?(reference_dir), "File does not exists #{reference_dir}")
+      reference_subdirs = [
+        "reference",
+        "reference1",
+        "reference2"
+      ]
+
+      # When
+      runner = AdLocalize::Runner.new
+      runner.run csv_files
+
+      # Then
+      reference_subdirs.each do |subdir|
+          all_files.each do |file|
+            reference_file = "#{reference_dir}/#{subdir}/#{file}"
+            generated_file = "exports/#{subdir}/#{file}"
+            assert(File.exist?(generated_file), "File does not exists #{generated_file}")
+            diff = Diffy::Diff.new(reference_file, generated_file, :source => 'files')
+            assert_empty(diff.to_s, "File #{generated_file} do not match reference. Diff: \n\n#{diff}\n")
+          end
+      end
   end
 
   private
 
-  def all_files
-    ios_files + android_files + json_files + yml_files + properties_files
+  def all_files(languages: ["fr", "en"])
+    ios_files(languages: languages) +
+    android_files(languages: languages) +
+    json_files(languages: languages) +
+    yml_files(languages: languages) +
+    properties_files(languages: languages)
   end
 
-  def ios_files(with_platform_directory: true)
+  def ios_files(with_platform_directory: true, languages: ["fr", "en"])
     files = ["InfoPlist.strings", "Localizable.strings", "Localizable.stringsdict"]
     languages
       .map { |language| "#{language}.lproj" }
@@ -72,27 +108,23 @@ class AdLocalizeTest < TestCase
       .map { |file| with_platform_directory ? "ios/#{file}" : file }
   end
 
-  def android_files
+  def android_files(languages: ["fr", "en"])
     languages
       .each_with_index
       .map { |language, i| i == 0 ? "values" : "values-#{language}" }
       .map { |language_folder| "android/#{language_folder}/strings.xml" }
   end
 
-  def json_files
+  def json_files(languages: ["fr", "en"])
     languages.map { |language| "json/#{language}.json" }
   end
 
-  def yml_files
+  def yml_files(languages: ["fr", "en"])
     languages.map { |language| "yml/#{language}.yml" }
   end
 
-
-  def properties_files
+  def properties_files(languages: ["fr", "en"])
     languages.map { |language| "properties/#{language}.properties" }
   end
 
-  def languages
-    ["fr", "en"]
-  end
 end
