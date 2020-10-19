a509bb810a8b974164f634fde3a7c41f7f33e575
Joanna Vigné <joanna.vigne@fabernovel.com>
2020-10-19 09:39:44 +0200

    remove csv file

--- lib/ad_localize/interactors/export_csv_files.rb
+++ lib/ad_localize/interactors/export_csv_files.rb
@@ -7,10 +7,8 @@ module AdLocalize
       end
 
       def call(export_request:)
-        wordings = export_request.csv_paths.map do |csv_path|
-          csv_file = WordingSources::CSVFile.new(path: csv_path)
-          @csv_file_to_wording.map(csv_file: csv_file)
-        end
+        LOGGER.debug("Starting export csv files : #{export_request.csv_paths.to_sentence}")
+        wordings = export_request.csv_paths.map { |csv_path| @csv_path_to_wording.map(csv_path: csv_path) }
         wording = @merge_wordings.call(wordings: wordings.compact, merge_policy: export_request.merge_policy)
         ExportWording.new.call(export_request: export_request, wording: wording)
       end
--- lib/ad_localize/mappers/csv_path_to_wording.rb
+++ lib/ad_localize/mappers/csv_path_to_wording.rb
@@ -1,13 +1,15 @@
 module AdLocalize
   module Mappers
-    class CSVFileToWording
-      def map(csv_file:)
-        return unless csv_file.valid?
+    class CSVPathToWording
+      def map(csv_path:)
+        @headers = CSV.foreach(csv_path).first
+        return unless valid?(csv_path: csv_path)
         translations = []
 
-        CSV.foreach(csv_file.path, headers: true, skip_blanks: true) do |row|
-          row_translations = map_row(row: row, locales: csv_file.locales)
+        CSV.foreach(csv_path, headers: true, skip_blanks: true) do |row|
+          row_translations = map_row(row: row, locales: locales)
           next if row_translations.blank?
+
           existing_keys = translations.map(&:key)
           new_translations = row_translations.reject do |translation|
             existing_keys.any? do |key|
@@ -21,10 +23,38 @@ module AdLocalize
         locale_wordings = translations.group_by(&:locale).map do |locale, group|
           Entities::LocaleWording.new(locale: locale, translations: group)
         end
-        Entities::Wording.new(locale_wordings: locale_wordings, default_locale: csv_file.locales.first)
+        Entities::Wording.new(locale_wordings: locale_wordings, default_locale: locales.first)
       end
 
       private
+
+      def valid?(csv_path:)
+        File.exist?(csv_path) && has_key_column? && has_locales?
+      end
+
+      def locales
+        @locales ||= compute_locales
+      end
+
+      def compute_locales
+        return [] unless has_key_column? && key_column_index < @headers.size.pred
+
+        @headers.slice(key_column_index.succ..-1).compact.reject do |header|
+          header.to_s.include?(Constant::COMMENT_KEY_COLUMN_IDENTIFIER)
+        end
+      end
+
+      def has_locales?
+        locales.present?
+      end
+
+      def has_key_column?
+        key_column_index.present?
+      end
+
+      def key_column_index
+        @key_column_index ||= @headers.index(Constant::CSV_WORDING_KEYS_COLUMN)
+      end
 
       def map_row(row:, locales:)
         csv_wording_key = row.field(Constant::CSV_WORDING_KEYS_COLUMN)
