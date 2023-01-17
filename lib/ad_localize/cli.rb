ac4031d8d42d4acae8bd9641a8b514d81a5e7a47
Joanna Vigne <joanna.vigne@outlook.fr>
2023-01-17 14:11:07 +0100

    allow exporting from csv files and sheets in a single command

--- lib/ad_localize/cli.rb
+++ lib/ad_localize/cli.rb
@@ -1,15 +1,19 @@
 module AdLocalize
   class CLI
     def self.start(args:)
+      downloaded_files = []
       options = OptionHandler.parse!(args)
       export_request = Mappers::OptionsToExportRequest.new.map(options: options)
-      if export_request.has_g_spreadsheet_options?
-        export_request.csv_paths = DownloadSpreadsheets.new.call(export_request: export_request)
-      else
-        export_request.csv_paths = options[:csv_paths]
+      if export_request.has_sheets?
+        downloaded_files = DownloadSpreadsheets.new.call(export_request: export_request)
+        export_request.csv_paths.concat(downloaded_files)
       end
-      
       Interactors::ExecuteExportRequest.new.call(export_request: export_request)
+    ensure
+      downloaded_files.each do |file|
+        file.close
+        file.unlink
+      end
     end
   end
 end
