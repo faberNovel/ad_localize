accd0e1caf85e90ca034df32404bcdca135b4e58
Joanna Vigne <joanna.vigne@fabernovel.com>
2023-01-18 14:33:27 +0100

    add missing wording type

---
+++ lib/ad_localize/entities/wording_type.rb
@@ -0,0 +1,10 @@
+module AdLocalize
+    module Entities
+        module WordingType
+          SINGULAR = 'singular'
+          PLURAL = 'plural'
+          INFO_PLIST = 'info_plist'
+          ADAPTIVE = 'adaptive'
+        end
+    end
+end
