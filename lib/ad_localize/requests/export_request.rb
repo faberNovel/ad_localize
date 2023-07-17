ed6910ca095e867349df79074cb72f25f03ff64e
Joanna Vigne <joanna.vigne@fabernovel.com>
2023-07-17 13:30:34 +0200

    add tests for export request

--- lib/ad_localize/requests/export_request.rb
+++ lib/ad_localize/requests/export_request.rb
@@ -32,17 +32,18 @@ module AdLocalize
       )
 
       def initialize(**args)
-        @locales = value_for(optional: args[:locales], default_value: DEFAULTS[:locales])
+        @locales = Array(value_for(optional: args[:locales], default_value: DEFAULTS[:locales]))
         @bypass_empty_values = value_for(optional: args[:bypass_empty_values], default_value: DEFAULTS[:bypass_empty_values])
-        @csv_paths = value_for(optional: args[:csv_paths], default_value: DEFAULTS[:csv_paths])
+        @csv_paths = Array(value_for(optional: args[:csv_paths], default_value: DEFAULTS[:csv_paths]))
         @merge_policy = value_for(optional: args[:merge_policy], default_value: DEFAULTS[:merge_policy])
         @output_path = Pathname.new(value_for(optional: args[:output_path], default_value: DEFAULTS[:output_path]))
-        @platforms = value_for(optional: args[:platforms], default_value: DEFAULTS[:platforms])
+        raw_platforms = value_for(optional: args[:platforms], default_value: DEFAULTS[:platforms])
+        @platforms = Array(raw_platforms) & Entities::Platform::SUPPORTED_PLATFORMS
         @spreadsheet_id = value_for(optional: args[:spreadsheet_id], default_value: DEFAULTS[:spreadsheet_id])
-        @sheet_ids = value_for(optional: args[:sheet_ids], default_value: DEFAULTS[:sheet_ids])
+        @sheet_ids = Array(value_for(optional: args[:sheet_ids], default_value: DEFAULTS[:sheet_ids]))
         @export_all = value_for(optional: args[:export_all], default_value: DEFAULTS[:export_all])
         @verbose = value_for(optional: args[:verbose], default_value: DEFAULTS[:verbose])
-        @downloaded_csvs = value_for(optional: args[:downloaded_csvs], default_value: DEFAULTS[:downloaded_csvs])
+        @downloaded_csvs = Array(value_for(optional: args[:downloaded_csvs], default_value: DEFAULTS[:downloaded_csvs]))
       end
 
       def value_for(optional:, default_value:)
--- test/requests/export_request_test.rb
+++ test/requests/export_request_test.rb
@@ -13,49 +13,35 @@ module AdLocalize
 
         request = ExportRequest.new(locales: nil)
         assert_empty request.locales
+
+        request = ExportRequest.new(locales: [])
+        assert_empty request.locales
       end
 
-      # test 'should get platforms' do
-      #   request = ExportRequest.new(platforms: "")
-      #   assert_equal Entities::Platform::SUPPORTED_PLATFORMS, request.platforms
+      test 'should detect if request bypasses empty values' do
+        request = ExportRequest.new(bypass_empty_values: true)
+        assert request.bypass_empty_values
 
-      #   request = ExportRequest.new(platforms: %w(ios android))
-      #   assert_equal %w(ios android), request.platforms
+        request = ExportRequest.new(bypass_empty_values: false)
+        assert_not request.bypass_empty_values
 
-      #   request = ExportRequest.new(platforms: 'json')
-      #   assert_equal %w(json), request.platforms
+        request = ExportRequest.new(bypass_empty_values: nil)
+        assert_not request.bypass_empty_values
 
-      #   request = ExportRequest.new(platforms: nil)
-      #   assert_empty request.platforms
-
-      #   request = ExportRequest.new(platforms: %w(foo bar ios))
-      #   assert_equal %w(ios), request.platforms
-      # end
-
-      test 'should get output path' do
-        request = ExportRequest.new(output_path: '')
-        assert_equal Pathname.new(ExportRequest::DEFAULTS[:output_path]), request.output_path
-
-        request = ExportRequest.new(output_path: 'foo')
-        assert_equal Pathname.new('foo'), request.output_path
-
-        request = ExportRequest.new(output_path: nil)
-        assert_equal Pathname.new(ExportRequest::DEFAULTS[:output_path]), request.output_path
+        request = ExportRequest.new(bypass_empty_values: '')
+        assert_not request.bypass_empty_values
       end
 
-      # test 'should get verbose' do
-      #   request = ExportRequest.new(verbose: nil)
-      #   refute request.verbose
+      test 'should get csv paths' do
+        request = ExportRequest.new(csv_paths: [])
+        assert_empty request.csv_paths
 
-      #   request = ExportRequest.new(verbose: "")
-      #   refute request.verbose
+        request = ExportRequest.new(csv_paths: %w[csv_1 csv_2])
+        assert_equal %w[csv_1 csv_2], request.csv_paths
 
-      #   request = ExportRequest.new(verbose: true)
-      #   assert request.verbose
-
-      #   request = ExportRequest.new(verbose: false)
-      #   refute request.verbose
-      # end
+        request = ExportRequest.new(csv_paths: nil)
+        assert_empty request.csv_paths
+      end
 
       test 'should get merge policy' do
         request = ExportRequest.new(merge_policy: nil)
@@ -74,54 +60,99 @@ module AdLocalize
         assert_equal 'foo', request.merge_policy
       end
 
-      # test 'should detect if request is valid with one csv' do
-      #   request = ExportRequest.new
-      #   assert_not request.valid?
+      test 'should get output path' do
+        request = ExportRequest.new(output_path: '')
+        assert_equal Pathname.new(ExportRequest::DEFAULTS[:output_path]), request.output_path
 
-      #   request = ExportRequest.new(csv_paths: %w(foo))
-      #   refute request.valid?
+        request = ExportRequest.new(output_path: 'foo')
+        assert_equal Pathname.new('foo'), request.output_path
 
-      #   request = ExportRequest.new(csv_paths: [__dir__ + "/../fixtures/reference.csv"])
-      #   assert request.valid?
+        request = ExportRequest.new(output_path: nil)
+        assert_equal Pathname.new(ExportRequest::DEFAULTS[:output_path]), request.output_path
+      end
 
-      #   request = ExportRequest.new(csv_paths: [__dir__ + "/../fixtures/reference.csv"], platforms: %w(foo))
-      #   refute request.valid?
-      # end
+      test 'should get platforms' do
+        request = ExportRequest.new(platforms: '')
+        assert_equal Entities::Platform::SUPPORTED_PLATFORMS, request.platforms
 
-      # test 'should detect if request is valid with one sheet' do
-      #   request = ExportRequest.new(spreadsheet_id: 'spreadsheet_id')
-      #   assert request.valid?
+        request = ExportRequest.new(platforms: %w(ios android))
+        assert_equal %w(ios android), request.platforms
 
-      #   request = ExportRequest.new(spreadsheet_id: 'spreadsheet_id', sheet_ids: %w(one))
-      #   assert request.valid?
-      # end
+        request = ExportRequest.new(platforms: 'json')
+        assert_equal %w(json), request.platforms
 
-      # test 'should detect if request is verbose' do
-      #   request = ExportRequest.new(verbose: nil)
-      #   refute request.verbose
+        request = ExportRequest.new(platforms: nil)
+        assert_equal Entities::Platform::SUPPORTED_PLATFORMS, request.platforms
 
-      #   request = ExportRequest.new(verbose: "")
-      #   refute request.verbose
+        request = ExportRequest.new(platforms: %w(foo bar ios))
+        assert_equal %w(ios), request.platforms
+      end
 
-      #   request = ExportRequest.new(verbose: true)
-      #   assert request.verbose
+      test 'should get spreadsheet id' do
+        request = ExportRequest.new(spreadsheet_id: '')
+        assert_nil request.spreadsheet_id
 
-      #   request = ExportRequest.new(verbose: false)
-      #   refute request.verbose
-      # end
+        request = ExportRequest.new(spreadsheet_id: nil)
+        assert_nil request.spreadsheet_id
 
-      test 'should detect if request bypasses empty values' do
-        request = ExportRequest.new(bypass_empty_values: true)
-        assert request.bypass_empty_values
+        request = ExportRequest.new(spreadsheet_id: 'spreadsheet_id')
+        assert_equal 'spreadsheet_id', request.spreadsheet_id
+      end
 
-        request = ExportRequest.new(bypass_empty_values: false)
-        assert_not request.bypass_empty_values
+      test 'should get sheet ids' do
+        request = ExportRequest.new(sheet_ids: '')
+        assert_equal ExportRequest::DEFAULTS[:sheet_ids], request.sheet_ids
 
-        request = ExportRequest.new(bypass_empty_values: nil)
-        assert_not request.bypass_empty_values
+        request = ExportRequest.new(sheet_ids: nil)
+        assert_equal ExportRequest::DEFAULTS[:sheet_ids], request.sheet_ids
 
-        request = ExportRequest.new(bypass_empty_values: '')
-        assert_not request.bypass_empty_values
+        request = ExportRequest.new(sheet_ids: [])
+        assert_equal ExportRequest::DEFAULTS[:sheet_ids], request.sheet_ids
+
+        request = ExportRequest.new(sheet_ids: %w[sheet_1 sheet_2])
+        assert_equal %w[sheet_1 sheet_2], request.sheet_ids
+      end
+
+      test 'should get export all' do
+        request = ExportRequest.new(export_all: nil)
+        assert_not request.export_all
+
+        request = ExportRequest.new(export_all: '')
+        assert_not request.export_all
+
+        request = ExportRequest.new(export_all: true)
+        assert request.export_all
+
+        request = ExportRequest.new(export_all: false)
+        assert_not request.export_all
+      end
+
+      test 'should get verbose' do
+        request = ExportRequest.new(verbose: nil)
+        assert_not request.verbose
+
+        request = ExportRequest.new(verbose: '')
+        assert_not request.verbose
+
+        request = ExportRequest.new(verbose: true)
+        assert request.verbose
+
+        request = ExportRequest.new(verbose: false)
+        assert_not request.verbose
+      end
+
+      test 'should get downloaded csvs' do
+        request = ExportRequest.new(downloaded_csvs: nil)
+        assert_empty request.downloaded_csvs
+
+        request = ExportRequest.new(downloaded_csvs: '')
+        assert_empty request.downloaded_csvs
+
+        request = ExportRequest.new(downloaded_csvs: [])
+        assert_empty request.downloaded_csvs
+
+        request = ExportRequest.new(downloaded_csvs: %w[csv_1 csv_2])
+        assert_equal %w[csv_1 csv_2], request.downloaded_csvs
       end
     end
   end
