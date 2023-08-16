36164f2d0a7a1bfab96cf2c73866c540dc545bbc
Alexandre Podlewski <alexandre.podlewski@fabernovel.com>
2023-08-16 17:47:52 +0200

    Add percent escaping in ios sanitizer to support % character in wording

--- lib/ad_localize/sanitizers/ios_sanitizer.rb
+++ lib/ad_localize/sanitizers/ios_sanitizer.rb
@@ -5,7 +5,10 @@ module AdLocalize
       def sanitize(value:)
         return if value.blank?
 
-        value.gsub(/(?<!\\)\"/, "\\\"")
+        processed_value = value.gsub(/(?<!\\)\"/, "\\\"")
+        # we should escape % sign when not used as formatting function (see: https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/Strings/Articles/formatSpecifiers.html)
+        processed_value = processed_value.gsub(/%(?!((\d+\$)?([a-zA-Z]|@)))/, '%%')
+        processed_value
       end
     end
   end
